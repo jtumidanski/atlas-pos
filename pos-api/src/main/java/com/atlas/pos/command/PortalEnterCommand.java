56f2fbb26addc5742e6c4db21d349491f75f3b8b
Tumidanski Justin D <Justin.Tumidanski@dematic.com>
2020-12-01 12:47:30 -0500

    Adjust attributes of PortalEnterCommand.

--- pos-api/src/main/java/com/atlas/pos/command/PortalEnterCommand.java
+++ pos-api/src/main/java/com/atlas/pos/command/PortalEnterCommand.java
@@ -1,4 +1,4 @@
 package com.atlas.pos.command;
 
-public record PortalEnterCommand(int mapId, int portalId, int characterId) {
+public record PortalEnterCommand(int worldId, int channelId, int mapId, int portalId, int characterId) {
 }
